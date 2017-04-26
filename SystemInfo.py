a58cf74315fe57863b74ca55e644bd36146a1bdb
JamesLinus <www.1464217939@outlook.com>
2017-04-27 06:07:38 +0800

    Add SystemInfo

---
+++ SystemInfo.py
@@ -0,0 +1,7 @@
+import psutil
+psutil.users()
+[user(name='giampaolo', terminal='pts/2', host='localhost', started=1340737536.0),
+ user(name='giampaolo', terminal='pts/3', host='localhost', started=1340737792.0)]
+
+psutil.boot_time()
+1365519115.0
